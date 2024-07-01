6dd8405312bb713dc35cbc4a4a356da0a3325a55
TINKU SINGH <154521515+Tinku-Singh1212@users.noreply.github.com>
2024-07-01 20:22:05 +0530

    01/07/24

---
+++ python/ass8.py
@@ -0,0 +1,13 @@
+class calculator:
+    x=int(input("enter the N1\n"))
+    y=int(input("enter the N2\n"))
+    def calculation(cal):
+        return cal
+        print(cal.x+cal.y)
+        print(cal.x-cal.y)
+        print(cal.x*cal.y)
+        print(cal.x/cal.y)
+obj=calculator()
+print(obj.calculation())
+
+
