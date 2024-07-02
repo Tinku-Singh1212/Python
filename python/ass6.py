7be74144c48e2cb3f21c5a714024c14800e153ef
TINKU SINGH <154521515+Tinku-Singh1212@users.noreply.github.com>
2024-07-02 21:19:01 +0530

    02/07/24

---
+++ python/ass6.py
@@ -0,0 +1,10 @@
+print("--------W H O---------")
+age=int(input("enter the age of child\n"))
+if ((age>=1) and (age<=5)):
+    print("Goto counter number 1 for polio")
+elif ((age>5) and (age<=10)):
+     print("Goto counter number 2 for vachine")
+elif ((age>10) and (age<=15)):
+    print("Goto counter number 3 for taking vetimans")     
+else:
+    print("beta ghar jao")   
