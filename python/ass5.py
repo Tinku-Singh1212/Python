b553a08027746f4dc6e66e82d5f008f31c419cc0
TINKU SINGH <154521515+Tinku-Singh1212@users.noreply.github.com>
2024-07-03 19:40:23 +0530

    03/07/24

---
+++ python/ass5.py
@@ -0,0 +1,12 @@
+a=int(input("enter the number"))
+for table in range(1,11):
+    print(a*table)
+
+'''a=int(input("enter the number"))
+for table in range(1,11,2):
+    print(a*table)'''
+
+'''a=int(input("enter the number"))
+for a in range(10):
+    print(a)'''
+
