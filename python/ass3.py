8f9d75a870bcc8abb0225710e1fc147a986d5bd8
TINKU SINGH <154521515+Tinku-Singh1212@users.noreply.github.com>
2024-07-03 19:45:17 +0530

    03/04/24

---
+++ python/ass3.py
@@ -0,0 +1,7 @@
+          #program of odd even vehical number
+print("welcome to Delhi")
+veh=int(input("enter the last four digit number of your vehical"))
+if (veh%2==0):
+    print("you are allowed to delhi")
+else:
+    print("you are not allowed to delhi")
