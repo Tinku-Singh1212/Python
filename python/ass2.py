f4c410250bdc017ec48f9012ecb7565aadf194ca
TINKU SINGH <154521515+Tinku-Singh1212@users.noreply.github.com>
2024-07-04 19:49:42 +0530

    04/07/24

---
+++ python/ass2.py
@@ -0,0 +1,20 @@
+print("1.addition 2.subtraction 3.multiplication 4.devision")
+def sum(a,b):
+    return sum
+a=int(input("enter the frist number\n"))
+b=int(input("enter the second number\n"))
+print("addition is =",a+b) 
+ 
+
+def sub(a,b):
+    return sub
+print("subtraction is=",a-b)     
+
+def mul(a,b):
+    return mul
+print("multiplication is =",a*b)     
+    
+def division(a,b): 
+    return division
+print("division is =",a/b)
+    
